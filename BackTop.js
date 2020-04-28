2fbdff31f197d6c73d402f1409a978f50aef5ee9
jayeshvaghela <vaghelaj@sheridancollege.ca>
2020-04-28 15:51:41 -0400

    Website Redesign 2020 V2

---
+++ BackTop.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('.back-top').fadeIn(); 
+        } else { 
+            $('.back-top').fadeOut(); 
+        } 
+    }); 
+    $('.back-top').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 1800); 
+        return false; 
+    }); 
+
+    
+});
